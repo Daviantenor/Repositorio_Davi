ec1c8bce71e21eaee87ec6227096f91309482bba
Daviantenor <daviantenor260@gmail.com>
2025-09-15 10:57:54 -0300

    Create frio.py

---
+++ Modulo 1/Codigos Basicos/frio.py
@@ -0,0 +1,23 @@
+nome=input(" nos fale seu nome: ")
+temperatura = float(input(f"{nome} nos informe a temperatura:"))
+
+
+if  20 <= temperatura <= 40:
+
+    print(f"tá um calor sufocante!")
+
+elif 20>  temperatura >= 5: 
+
+    print(f"tá frio")
+
+elif 0 < temperatura < 5:
+
+    print(f"O frio ta congelante")
+
+else:
+
+    print(f"morto de frio")
+
+    temperatura >=20 and temperatura <=40:
+
+    print("esta quente")
