5cdaba23775f71e0903bea5428fd51661e312a1b
Daviantenor <daviantenor260@gmail.com>
2025-09-15 11:16:54 -0300

    Create move_mouse.py

---
+++ Modulo 2/Exercicios/move_mouse.py
@@ -0,0 +1,29 @@
+import pyautogui as pag
+
+pag.sleep(2)
+pag.press("win")
+pag.write("in instragam")
+pag.press("enter")
+
+pag.sleep(2)
+# pag.mouseInfo()
+pag.sleep(2)
+pag.moveTo(238,377)
+pag.sleep(2)
+pag.click()
+pag.sleep(2)
+# pag.mouseInfo()
+pag.moveTo(1127,392)
+
+pag.click()
+
+pag.write("xxxx")
+pag.sleep(2)
+# pag.mouseInfo()
+pag.moveTo(1141,448)
+pag.click()
+pag.write("xxxxxxx")
+pag.sleep(1.5)
+# pag.mouseInfo()
+pag.moveTo(1235,485)
+pag.click()
