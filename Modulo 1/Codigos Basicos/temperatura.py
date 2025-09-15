35286f5367ed155402a29be065c50e69c7d1bcfd
Daviantenor <daviantenor260@gmail.com>
2025-09-15 10:57:06 -0300

    Create temperatura.py

---
+++ Modulo 1/Codigos Basicos/temperatura.py
@@ -0,0 +1,18 @@
+sistema_de_temperatura=float(input ( "me informe a temperatura:"))
+
+
+if  sistema_de_temperatura >=20:
+
+    print(f"tá um calor sufocante!")
+
+elif sistema_de_temperatura <=20: 
+
+    print(f"tá frio")
+
+elif  sistema_de_temperatura <=5:
+
+    print(f"O frio ta congelando")
+
+else:
+
+    print(f"morto de frio")
