560d17301cf677bce252c965f3a9beec363c2a2d
Daviantenor <daviantenor260@gmail.com>
2025-09-15 10:50:02 -0300

    Create nome_salgadinho.py

---
+++ Modulo 1/Codigos Basicos/nome_salgadinho.py
@@ -0,0 +1,3 @@
+nome_salgadinho=input("Nome do salgadinho:") preço_salgadinho=float(input("Preço do salgadinho:")) peso_salgadinho=float(input("Peso do salgadinho:"))
+
+print("-----------------------------------") print(f"|salgadinho:,{nome_salgadinho}") print(f"|preço:,{preço_salgadinho}") print(f"|peso:,{peso_salgadinho}") print("-----------------------------------")
