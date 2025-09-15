a34fb91525640b5b5c85fe3367810b81592628ac
Daviantenor <daviantenor260@gmail.com>
2025-09-15 10:41:57 -0300

    Create orientação_imc.py

---
+++ Modulo 1/Codigos Basicos/orientação_imc.py
@@ -0,0 +1,10 @@
+criar um Imc que faça em loop para registrar varios clientes , perguntando nome ,peso e altura 
+
+apresentar uma resposta sobre o peso dele 
+
+
+
+
+
+
+imc=peso / (altura * altura)
